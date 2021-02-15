aaa515a78b657b03bc66240167d98fdfb5ada475
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-02-15 21:08:30 +0330

    application model

---
+++ src/models/Application.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const ApplicationSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  categori: { type: String, required: true },
+  tag: { type: String, required: true, enum: ["driver", "game", "etc","food"] },
+  size: { type: String, required: true },
+  images: { type: Object, default: {} },
+  rate: { type: String, default: "4" },
+  companyName: { type: String, required: true },
+  version: { type: String, default: "1" },
+  installNumber: { type: String },
+  comments: { type: Object, default: {} },
+  description: { type: String, required: true },
+});
+const ApplicationModel = mongoose.model("application", ApplicationSchema);
+module.exports = ApplicationModel;
