2057f48a1a929df5a8cd94779af646666d64b352
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-02-15 22:19:57 +0330

    refactor register to another file

--- src/routes/apis/applications.js
+++ src/routes/apis/applications.js
@@ -1,44 +1,9 @@
 const express = require("express");
-const { validationResult } = require("express-validator");
-const appValidation = require("../../middlewares/fieldValidation/application");
 const router = express.Router();
 const ApplicationModel = require("../../models/Application");
+const registerApplication = require("./registerApplication");
+const getAllApplications=require('./getApplications')
 // router.post("/register", appValidation(), (req, res) => {
-router.post("/register", (req, res) => {
-  const {
-    name,
-    categori,
-    tag,
-    size,
-    images,
-    rate,
-    companyName,
-    version,
-    installNumber,
-    comments,
-    description,
-  } = req.body;
-  //errors
-  //   const errors = validationResult(req);
-  //   const errs = [];
-  //   if (errors.length !== 0) {
-  //     errors.array().map((item) => errs.push(item.msg));
-  //     res.status(400).json("we have error");
-  //   }
-  const newApplication = new ApplicationModel({
-    name,
-    categori,
-    tag,
-    size,
-    images,
-    rate,
-    companyName,
-    version,
-    installNumber,
-    comments,
-    description,
-  });
-  newApplication.save(opt=>console.log(opt));
-  res.status(200).json("ok");
-});
+router.use("/register", registerApplication);
+router.use("/get", getAllApplications);
 module.exports = router;
