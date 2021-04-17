085347bf4c121d8d250080c763982ddf0f5b16e9
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-04-17 23:09:41 +0430

    add name for comment

--- src/routes/apis/addComment.js
+++ src/routes/apis/addComment.js
@@ -4,14 +4,14 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  const { _id, text } = req.body;
+  const { _id, text, name } = req.body;
   ApplicationSchema.find({ _id }, (error, doc) => {
     if (error) throw new Error(error);
     const application = doc[0];
     const { comments } = application;
     application.comments = [
       ...comments,
-      { _id: Math.random() * 100000000000, text },
+      { _id: Math.random() * 100000000000, text, name },
     ];
     application.save((err) => {
       if (err) throw new Error(err);
