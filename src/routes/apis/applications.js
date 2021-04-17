d7e44dfd9fba248c631fbd7595eab12e0d5a9361
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-04-17 22:48:56 +0430

    add comments

--- src/routes/apis/applications.js
+++ src/routes/apis/applications.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const ApplicationModel = require("../../models/Application");
 const registerApplication = require("./registerApplication");
 const getAllApplications=require('./getApplications')
+const addComment=require('../apis/addComment');
 // router.post("/register", appValidation(), (req, res) => {
 router.use("/register", registerApplication);
 router.use("/get", getAllApplications);
+router.use("/add-comment",addComment)
 module.exports = router;
