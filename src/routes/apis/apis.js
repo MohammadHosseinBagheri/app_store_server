5063a438d0110b6d116b96af5b4ceb05fe0af0c5
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-05-01 23:02:55 +0430

    add signin signup peyment

--- src/routes/apis/apis.js
+++ src/routes/apis/apis.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const applicationsApi = require("./applications");
+const signinApi=require('./auth/signin')
+const signupApi=require('./auth/signup')
+const peyment=require("./peyment")
 const cors = require("cors");
 router.use("/applications", cors(), applicationsApi);
+router.use("/signin", cors(), signinApi);
+router.use("/signup", cors(), signupApi);
+router.use("/peyment", cors(), peyment);
 module.exports = router;
