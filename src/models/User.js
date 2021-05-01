095b65c755e079878985ed9ae20724abe439b985
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-05-01 23:02:33 +0430

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const UserSchema = mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+});
+const UserModel = mongoose.model("user", UserSchema);
+module.exports = UserModel;
