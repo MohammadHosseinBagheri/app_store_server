42edf822d1508269a56e7dc0c9c9f3e42c08182c
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-05-15 23:09:20 +0430

    test cors

--- src/routes/apis/applications.js
+++ src/routes/apis/applications.js
@@ -2,12 +2,19 @@ const express = require("express");
 const router = express.Router();
 const ApplicationModel = require("../../models/Application");
 const registerApplication = require("./registerApplication");
-const getAllApplications=require('./getApplications')
-const addComment=require('../apis/addComment');
+const getAllApplications = require("./getApplications");
+const addComment = require("../apis/addComment");
 const cors = require("cors");
 
 // router.post("/register", appValidation(), (req, res) => {
 router.use("/register", registerApplication);
 router.use("/get", getAllApplications);
-router.use("/add-comment",cors(),addComment)
+const test = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
+};
+router.use("/add-comment", test, addComment);
 module.exports = router;
